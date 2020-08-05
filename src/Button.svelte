bafcc0cd2924f4a82fdb7d1ddace55e2acda868d
Dillon Fagan <dillonfagan@pm.me>
2020-08-04 21:09:25 -0400

    Pill Buttons

--- src/Button.svelte
+++ src/Button.svelte
@@ -2,4 +2,4 @@
     export let text;
 </script>
 
-<button class="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">{text}</button>
+<button class="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full">{text}</button>
