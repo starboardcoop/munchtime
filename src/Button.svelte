38d17ef2b6374e3d48dc168bc4ff694f2c3f1340
Dillon Fagan <dillonfagan@pm.me>
2020-08-07 12:43:07 -0400

    Add image to ItemCard

--- src/Button.svelte
+++ src/Button.svelte
@@ -2,4 +2,8 @@
     export let text;
 </script>
 
-<button class="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full">{text}</button>
+<button class="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4">
+    <svg class="fill-current h-8 w-8" viewBox="0 0 20 20">
+        <path fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clip-rule="evenodd" />
+    </svg>
+</button>
--- src/ItemCard.svelte
+++ src/ItemCard.svelte
@@ -7,13 +7,14 @@
     export let unit;
 </script>
 
-<div class="p-4 rounded bg-white shadow hover:shadow-md mb-4 max-w-sm">
-    <div class="mb-4">
+<div class="rounded overflow-hidden bg-white shadow hover:shadow-lg mb-4 max-w-sm">
+    <img src="/images/burger.jpg" alt="Burger" />
+    <div class="p-4 mb-4">
         <h2 class="font-display text-2xl text-red-600">{name}</h2>
         <div class="text-gray-500">from {maker}</div>
         <div>${price} per {unit}</div>
     </div>
-    <div>
+    <div class="flex flex-col items-end">
         <Button text="Order" />
     </div>
 </div>
