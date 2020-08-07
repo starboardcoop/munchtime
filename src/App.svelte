b7ae81d57951c9f973b0da9d1d610091d3f71365
Dillon Fagan <dillonfagan@pm.me>
2020-08-07 18:59:14 -0400

    Move items into items.js

--- src/App.svelte
+++ src/App.svelte
@@ -1,44 +1,7 @@
 <script>
 	import Tailwind from "./Tailwind.svelte"
 	import ItemCard from "./ItemCard.svelte"
-
-	const items = [
-		{
-			name: "Jumbo Burger",
-			from: "Hamburgerama",
-			price: 10,
-			unit: "burger",
-			imageSource: "/images/burger.jpg"
-		},
-		{
-			name: "Clam Chowder",
-			from: "Local Clam Shack",
-			price: 10,
-			unit: "bowl",
-			imageSource: "/images/chowder.jpg"
-		},
-		{
-			name: "BBQ Chicken Nuggets",
-			from: "Bob's BBQ",
-			price: 15,
-			unit: "12 nuggets",
-			imageSource: "/images/nuggets.png"
-		},
-		{
-			name: "Spanish Omelette",
-			from: "El Restaurante Español",
-			price: 10,
-			unit: "tortilla",
-			imageSource: "/images/tortilla.jpg"
-		},
-		{
-			name: "California Roll",
-			from: "Little Tokyo",
-			price: 10.50,
-			unit: "10 pieces",
-			imageSource: "/images/sushi.jpg"
-		}
-	]
+	import { items } from "./items"
 </script>
 
 <Tailwind />
