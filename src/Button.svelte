6ea1c8ec7db877d9a42c2569693762137ed45eba
Dillon Fagan <dillonfagan@pm.me>
2020-08-07 19:01:05 -0400

    Add rounded corner to Button

--- src/Button.svelte
+++ src/Button.svelte
@@ -4,7 +4,7 @@
     const toggleSelected = () => selected = !selected;
 </script>
 
-<button class="bg-red-600 hover:bg-red-700 text-white py-2 px-4" class:selected={selected} on:click={toggleSelected}>
+<button class="bg-red-600 hover:bg-red-700 text-white py-2 px-4 rounded-tl" class:selected={selected} on:click={toggleSelected}>
     <svg class="fill-current h-8 w-8" viewBox="0 0 20 20" class:hidden={selected}>
         <path fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clip-rule="evenodd" />
     </svg>
