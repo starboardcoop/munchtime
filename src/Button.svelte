63057e8549a156403ccdbfc15feecc35860391df
Dillon Fagan <dillonfagan@pm.me>
2020-08-07 18:47:23 -0400

    Add checkmark to Button when selected

--- src/Button.svelte
+++ src/Button.svelte
@@ -5,17 +5,24 @@
 </script>
 
 <button class="bg-red-600 hover:bg-red-700 text-white py-2 px-4" class:selected={selected} on:click={toggleSelected}>
-    <svg class="fill-current h-8 w-8" viewBox="0 0 20 20">
+    <svg class="fill-current h-8 w-8" viewBox="0 0 20 20" class:hidden={selected}>
         <path fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clip-rule="evenodd" />
+    </svg>
+    <svg class="fill-current h-8 w-8" viewBox="0 0 20 20" class:hidden={!selected}>
+        <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
     </svg>
 </button>
 
 <style>
     .selected {
-        @apply bg-blue-500;
+        @apply bg-green-500;
     }
 
     .selected:hover {
-        @apply bg-blue-600;
+        @apply bg-green-600;
+    }
+
+    .hidden {
+        @apply hidden;
     }
 </style>
